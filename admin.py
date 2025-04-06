be2f05585d02bd1e75f114208ae40170170a90fb
Sanjita Ray <sanjitaray62@gmail.com>
2025-04-06 17:47:01 +0530

    edited

---
+++ admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+# Register your models here.
+from login_sys.models import details
+admin.site.register(details)
--- login_sys/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from login_sys.models import details
-admin.site.register(details)
